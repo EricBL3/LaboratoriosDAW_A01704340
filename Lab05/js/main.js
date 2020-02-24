639141d9a5c88a31bd9b0ae13724be5aabaca17b
Eric Buitrón López <A01704340@itesm.mx>
2020-02-23 18:53:26 -0600

    agregar paginas de ejercicios

---
+++ Lab05/js/main.js
@@ -0,0 +1,14 @@
+document.getElementById("passwords").onclick = function()
+{
+	open("ejercicios/passwords.html", "_self");
+}
+
+document.getElementById("vender").onclick = function()
+{
+	open("ejercicios/venta.html", "_self");
+}
+
+document.getElementById("videojuegos").onclick = function()
+{
+	open("ejercicios/videojuegos.html", "_self");
+}
