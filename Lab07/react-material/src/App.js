bdaf1928979774a0b34bbcb15a636a68ab22cdc6
Eric Buitrón López <A01704340@itesm.mx>
2020-03-01 14:00:01 -0600

    setup project with example

---
+++ Lab07/react-material/src/App.js
@@ -0,0 +1,27 @@
+// App.js
+
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import Bookmarks from '@material-ui/icons/Bookmarks';
+import TextField from '@material-ui/core/TextField';
+import Navbar from './navbar';
+import MediaCard from './card';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Button variant="outlined" color="primary">
+          <Bookmarks></Bookmarks>
+        </Button> <br/>
+        <TextField placeholder="Placeholder here" 
+        label="Basic TextField" />
+        <MediaCard />
+        
+      </div>
+    );
+  }
+}
+
+export default App;
