e076f2ac06ccdb2d5581ae769dd03c570a074f8e
Eric Buitrón López <A01704340@itesm.mx>
2020-03-09 21:02:01 -0600

    acabar función mediana

--- Lab09/index.php
+++ Lab09/index.php
@@ -6,8 +6,29 @@
 	#Función promedios
 	$arreglo1 = array(1,2,3,4,5);
 	$arreglo2 = array(90, 80, 183, 87, 32, 19);
-	$promedio1 = array_sum($arreglo1)/count($arreglo1);
-	$promedio2= array_sum($arreglo2)/count($arreglo2);
+
+	function promedio($arr)
+	{
+		return array_sum($arr)/count($arr);
+	}
+	$promedio1 = promedio($arreglo1);
+	$promedio2= promedio($arreglo2);
+
+	#Función mediana
+	function median($arr)
+	{
+		sort($arr);
+		$count = count($arr);
+		$index = floor($count/2);
+		if($count & 1)
+			return $arr[$index];
+		else
+			return ($arr[$index-1] + $arr[$index])/2;
+	}
+	$mediana1 = median($arreglo1);
+	$mediana2 = median($arreglo2);
+
+
 
 	include("partials/_funciones.html");
 	include("partials/_preguntas.html");
