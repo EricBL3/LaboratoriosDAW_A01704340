a541b794745153f352ca56ee0693a2e67fcc71e3
Eric Buitrón López <A01704340@itesm.mx>
2020-03-09 20:41:53 -0600

    acabar función promedios

--- Lab09/index.php
+++ Lab09/index.php
@@ -3,7 +3,11 @@
 
 	include("partials/_nav.html");
 
-	
+	#Función promedios
+	$arreglo1 = array(1,2,3,4,5);
+	$arreglo2 = array(90, 80, 183, 87, 32, 19);
+	$promedio1 = array_sum($arreglo1)/count($arreglo1);
+	$promedio2= array_sum($arreglo2)/count($arreglo2);
 
 	include("partials/_funciones.html");
 	include("partials/_preguntas.html");
