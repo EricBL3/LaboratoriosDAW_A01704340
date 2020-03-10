b914c7ef9011c9c7a4d347c751a88f5316dd34d5
Eric Buitrón López <A01704340@itesm.mx>
2020-03-09 21:57:00 -0600

    acabar lista y tabla de números

--- Lab09/index.php
+++ Lab09/index.php
@@ -28,6 +28,30 @@
 	$mediana1 = median($arreglo1);
 	$mediana2 = median($arreglo2);
 
+	#función lista
+	$menor_mayor1 = $arreglo1;
+	sort($menor_mayor1);
+	$mayor_menor1 = $arreglo1;
+	arsort($mayor_menor1);
+	$menor_mayor2 = $arreglo2;
+	sort($menor_mayor2);
+	$mayor_menor2 = $arreglo2;
+	arsort($mayor_menor2);
+
+	#función tabla
+	function crearTabla($n)
+	{
+		$data = array();
+		for($i = 1; $i <= $n; $i++)
+		{
+			array_push($data, array("n"=>$i, "n²"=>pow($i, 2), "n³"=>pow($i,3)));
+		}
+		return $data;
+	}
+	$datos1 = crearTabla(5);
+	$datos2 = crearTabla(10);
+
+
 
 
 	include("partials/_funciones.html");
