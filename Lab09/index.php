11c1468e88661384b9bef764aca4f990f5174498
Eric Buitrón López <A01704340@itesm.mx>
2020-03-09 20:12:36 -0600

    empezar seccion funciones

--- Lab09/index.php
+++ Lab09/index.php
@@ -2,9 +2,12 @@
 	include("_header.html");
 
 	include("partials/_nav.html");
+
+	
+
 	include("partials/_funciones.html");
 	include("partials/_preguntas.html");
 	include("partials/_referencias.html");
-	
+
 	include("_footer.html");
 ?>
