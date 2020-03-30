1121cbc2657d88bdb81b809de557affd6ce5b92d
Eric Buitrón López <A01704340@itesm.mx>
2020-03-29 23:39:22 -0600

    agregar partials de navegación, preguntas y referencias

---
+++ Lab14/index.php
@@ -0,0 +1,10 @@
+<?php
+
+	include("_header.html");
+	include("partials/_nav.html");
+
+
+	include("partials/_preguntas.html");
+	include("partials/_referencias.html");
+	include("_footer.html");
+?>
