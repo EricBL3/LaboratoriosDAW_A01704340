b161e3cc63e1b13e0045bc817f42a6ac61702fa1
Eric Buitrón López <A01704340@itesm.mx>
2020-03-30 18:02:38 -0600

    agregar funciones para conectarse y desconectarse de la bd

--- Lab14/model.php
+++ Lab14/model.php
@@ -1,4 +1,16 @@
 <?php
+	function conectar_bd()
+	{
+		$conexion_bd = mysqli_connect("localhost", "root", "", "Lab14");
+		if($conexion_bd == NULL)
+			die("La base de datos Lab14 está en mantenimiento, vuelve a intentarlo más tarde...");
+		
+		return $conexion_bd;
+	}
 
+	function desconectar_bd($conexion_bd)
+	{
+		mysqli_close($conexion_bd);
+	}
 
 ?>
