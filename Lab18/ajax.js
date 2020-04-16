2baf779530a49e167585e1ab22a8001455a99b10
Eric Buitrón López <A01704340@itesm.mx>
2020-04-16 18:07:21 -0500

    agregar ajax.js

---
+++ Lab18/ajax.js
@@ -0,0 +1,34 @@
+function getRequestObject()
+{
+	if(window.XMLHttpRequest)
+		return (new XMLHttpRequest);
+	else if(window.ActiveXObject)
+	{
+		//IE 6-
+		return (new ActiveXObject("Microsoft.XMLHTTP"));
+	}
+	else //no ajax
+		return (null);
+}
+
+function sendRequest()
+{
+	request = getRequestObject();
+	if(request != null)
+	{
+		let userInput = document.getElementById("userInput");
+		let url = 'ssajax.php?pattern='+userInput.value;
+		
+		request.open('GET', url, true);
+		request.onreadystatechange = 
+			function(){
+				if(request.readyState == 4)
+				{
+					let ajaxResponse = document.getElementById("ajaxResponse");
+					ajaxResponse.innerHTML = request.responseText;
+					ajaxResponse.style.visibility = "visible";
+				}
+			};
+		request.send(null);
+	}
+}
