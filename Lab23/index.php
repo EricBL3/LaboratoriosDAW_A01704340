e63e4db81f8915ab0bd991301dbe993783c830c0
Eric Buitrón López <A01704340@itesm.mx>
2020-05-16 14:22:17 -0500

    empezar lab 23

--- Lab23/index.php
+++ Lab23/index.php
@@ -1,7 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
 <?php
 //MY TAG - 8ULJ88YQ
 $clantag = "#2P2CJJ2CL";
@@ -30,69 +26,21 @@ if (isset($data["reason"])) {
 
 $members = $data["memberList"];
 
+if (isset($errormsg)) {
+  echo "<p>", "Failed: ", $data["reason"], " : ", isset($data["message"]) ? $data["message"] : "", "</p></body></html>";
+  exit;
+}
+
+include("_header.html");
+
+include("partials/_nav.html");
+include("partials/tabla.html");
+
+include("partials/_preguntas.html");
+
+include("partials/_referencias.html");
+
+include("_footer.html");
+
+
 ?>
-  <title><?php echo $data["name"]; ?></title>
-</head>
-<body>
-<?php
-  if (isset($errormsg)) {
-    echo "<p>", "Failed: ", $data["reason"], " : ", isset($data["message"]) ? $data["message"] : "", "</p></body></html>";
-    exit;
-  }
-?>
-  <table border="1">
-    <tr>
-      <td rowspan="11">Clan level : <?php echo $data["clanLevel"]; ?><br/><img src="<?php echo $data["badgeUrls"]["medium"]; ?>" alt="<?php echo $data["name"]; ?>"/></td>
-      <td><?php echo $data["name"]; ?></td><td><?php echo $data["tag"]; ?></td>
-      <td rowspan="11"><?php echo $data["description"]; ?></td>
-    </tr>
-    <tr>
-      <td>Total points</td><td><?php echo $data["clanPoints"]; ?></td>
-    </tr>
-    <tr>
-      <td>Wars won</td><td><?php echo $data["warWins"]; ?></td>
-    </tr>
-    <tr>
-      <td>War win streak</td><td><?php echo $data["warWinStreak"]; ?></td>
-    </tr>
-    <tr>
-      <td>Wars drawn</td><td><?php echo $data["warTies"]; ?></td>
-    </tr>
-    <tr>
-      <td>Wars lost</td><td><?php echo $data["warLosses"]; ?></td>
-    </tr>
-    <tr>
-      <td>Members</td><td><?php echo $data["members"]; ?>/50</td>
-    </tr>
-    <tr>
-      <td>Type</td><td><?php echo $data["type"]; ?></td>
-    </tr>
-    <tr>
-      <td>Required trophies</td><td><?php echo $data["requiredTrophies"]; ?></td>
-    </tr>
-    <tr>
-      <td>War frequency</td><td><?php echo $data["warFrequency"]; ?></td>
-    </tr>
-    <tr>
-      <td>Clan location</td><td><?php echo $data["location"]["name"]; ?></td>
-    </tr>
-  </table>
-  <table border="1">
-<?php
-  foreach ($members as $member) {
-?>
-    <tr>
-      <td><?php echo $member["clanRank"], "(", $member["previousClanRank"], ")"; ?></td>
-      <td><img src="<?php echo $member["league"]["iconUrls"]["tiny"]; ?>" alt="<?php echo $member["league"]["name"]; ?>"/></td>
-      <td><?php echo $member["expLevel"]; ?></td>
-      <td><?php echo "<b>", $member["name"], "</b><br/>", $member["role"]; ?></td>
-      <td>Donated:<br/><?php echo $member["donations"]; ?></td>
-      <td>Received:<br/><?php echo $member["donationsReceived"]; ?></td>
-      <td><?php echo $member["trophies"]; ?></td>
-    </tr>
-<?php
-  }
-?>
-  </table>
-</body>
-</html>
